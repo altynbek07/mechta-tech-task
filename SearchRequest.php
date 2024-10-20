eb004947a2b2cf8768f751bde2a8782c3be3fc68
Altynbek <altynbek.kazezov.97@gmail.com>
2024-10-21 02:38:06 +0500

    Create SearchRequest.php

---
+++ SearchRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace vBulletin\Search;
+
+class SearchRequest
+{
+    private ?int $searchId;
+    private ?string $query;
+
+    public function __construct(array $requestData)
+    {
+        $this->searchId = isset($requestData['searchid']) ? (int)$requestData['searchid'] : null;
+        $this->query = $requestData['q'] ?? null;
+    }
+
+    public function getSearchId(): ?int
+    {
+        return $this->searchId;
+    }
+
+    public function getQuery(): ?string
+    {
+        return $this->query;
+    }
+}
