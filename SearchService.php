63b1650c1cb1deda6aeaf3ee472623bccbe29137
Altynbek <altynbek.kazezov.97@gmail.com>
2024-10-21 02:33:12 +0500

    Create SearchService.php

---
+++ SearchService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace vBulletin\Search;
+
+use Psr\Log\LoggerInterface;
+
+class SearchService
+{
+    public function __construct(
+        private SearchRepository $searchRepository,
+        private LoggerInterface $logger,
+        private RendererInterface $renderer
+    ) {
+    }
+
+    public function handleSearch(SearchRequest $request): void
+    {
+        if ($request->getSearchId()) {
+            $results = $this->searchRepository->getResultsBySearchId($request->getSearchId());
+        } elseif ($request->getQuery()) {
+            $results = $this->searchRepository->getResultsByQuery($request->getQuery());
+            $this->logger->info('Search query: ' . $request->getQuery());
+        } else {
+            $this->renderer->renderSearchForm();
+            return;
+        }
+
+        $this->renderer->renderSearchResults($results);
+    }
+}
